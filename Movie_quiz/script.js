89ac183fdfb2cb6cfe19cf4a64e20e9c8da914a7
sreevishnu-ux <sreevishnuvinod@gmail.com>
2021-06-26 22:16:47 +0530

    first commit

---
+++ Movie_quiz/script.js
@@ -0,0 +1,47 @@
+const correctAnswers =  ['A', 'A', 'A', 'A'];
+
+const form = document.querySelector('.quiz-form');
+
+const result  = document.querySelector('.result');
+
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+
+    let score = 0;
+
+    const userAnswers = [form.Q1.value,form.Q2.value, form.Q3.value, form.Q4.value];
+
+    //to check answers
+
+    userAnswers.forEach((answer, index) => {
+        if(answer === correctAnswers[index]){
+            score += 25;
+        }
+
+        console.log(score);
+
+
+    });
+
+    //show result on page
+    scrollTo(0,0);
+    result.querySelector('span').textContent = `${score}%`;
+    result.classList.remove('d-none');
+
+    let output = 0;
+    const timer = setInterval(() => {
+        result.querySelector('span').textContent = `${output}%`;
+
+        if(output === score){
+            clearInterval(timer);
+        }
+        else{
+            output++;
+        }
+
+    },30);
+});
+
+
