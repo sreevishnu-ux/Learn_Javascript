60fd2a79e80e61ccf410febbc6e77b39b014dfa2
sreevishnu-ux <sreevishnuvinod@gmail.com>
2021-06-30 11:14:18 +0530

    added search filtering

--- Todo List/app.js
+++ Todo List/app.js
@@ -1,33 +1,57 @@
-const addform = document.querySelector('.add');
-const list = document.querySelector('.todos');
-//const delform = document.querySelector('.')
- 
-const generateTemplate = todos => {
+const formadd=document.querySelector('.add');
+const list=document.querySelector('.todos')
+const search=document.querySelector('.search input');
 
-    const html = `
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-        <span> ${todos}</span>
-        <i class="far fa-trash-alt delete"></i>
-        </li>
-    `;
-    list.innerHTML += html ;
+const generatetemplate= todo =>{
+
+    const html=`
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+        <span>${todo}</span>
+        <i class="fas fa-trash delete"></i>
+    </li> `
+
+    list.innerHTML +=html;
 };
 
 
-addform.addEventListener('submit' , e =>{
-
+formadd.addEventListener('submit',e =>{
     e.preventDefault();
+    const form=formadd.add.value.trim();
 
-    const todos = addform.add.value.trim();
-
-    generateTemplate(todos);
+    if(form.length)
+    {
+        generatetemplate(form);
+        formadd.reset();
+    }
 });
 
-//delete todos
-list.addEventListener('click', e =>{
-    if(e.target.classList.contains('delete')){
-        e.target.parentElement.remove();
-    }
 
+list.addEventListener('click' , e =>{
+
+    console.log(e.target.classList);
+    if(e.target.classList.contains('delete'));
+    {
+        e.target.parentElement.remove(); 
+    }
+        
+});
+
+const filtertodos=(term)=>{
+
+    Array.from(list.children)
+    .filter ((todo)=>  !todo.textContent.toLowerCase().includes(term))
+    .forEach((todo)=>todo.classList.add('filtered'));
+
+    Array.from(list.children)
+    .filter ((todo)=>  todo.textContent.toLowerCase().includes(term))
+    .forEach((todo)=>todo.classList.remove('filtered'));
+
+};
+
+
+search.addEventListener('keyup',()=>{
+     
+    const term=search.value.trim().toLowerCase();
+    filtertodos(term);
 
 });
